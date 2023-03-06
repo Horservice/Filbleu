c26fbe0854e89742475a5f4364d0e343a3d805a0
Horservice <helleringer.simon@hotmail.com>
2023-03-06 16:21:18 +0100

    2

---
+++ front/form(formulairedecontact).php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=7, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="styleform.css"/> -->
+    <title>Homepage - Nomdusite</title>
+
+</head>
+<style>
+
+    .a{
+
+        background-color: red;
+
+    }
+
+    .b{
+
+        background-color: blue;
+
+    }
+    .c{
+
+        background-color: cadetblue;
+
+    }
+
+    .d{
+
+        background-color: darkorange;
+
+
+    }
+
+    
+
+
+</style>
+<body>
+<main>
+
+    <div class="container-fluid a">
+
+        <div class="container b">
+
+                <h1 class="text-center text-white" >Nous contacter</h1>
+
+            <div class="container c">
+
+
+                <h2 class="text-white text-center">Formulaire</h2>
+
+
+                <form action="" method="POST" class="row" enctype="multipart/form-data">
+                    <div class="mb-3">
+                        <label for="" class="form-label">Nom (obligatoire)</label>
+                        <input type="text" class="form-control"  name="">
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="" class="form-label">Email (obligatoire)</label>
+                        <input type="email" class="form-control"  name="">
+                    </div>
+
+                    
+                    <div class="mb-3">
+                        <label for="" class="form-label">Votre message</label>
+                        <textarea placeholder="" name="msg" class="form-control"></textarea>              
+                    </div>
+        
+        
+                    <div class="mb-3 col-3">
+                        <button type="submit" class="btn btn-primary" name="submit">Valider</button>
+                    </div>
+        
+        
+        
+        
+                </form>
+        
+        
+
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+        </div>
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</main>
+</body>
+</html>
